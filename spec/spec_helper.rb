c2eb0a72ed4d10746aa3320ee2a786f742657b75
Adam <adamgsteel@gmail.com>
2016-01-22 13:58:15 -0700

    [style] spacing in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'rails_db_protect'
 require 'rake'
-
 require 'support/rails.rb'
 require 'support/activesupport-stringinquirer.rb'
+
 load 'support/Rakefile'
 load 'rails_db_protect/Rakefile'
